9e6c842771bcf69ed46b9e2ec59a87bf43ae09c4
Sand <sandboxasdf@gmail.com>
2024-06-30 18:38:58 +1000

    Close button now works

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog
 
 const myLibrary = [];
+var booksMade = 0;
 
 function Book(title, author, pageCount, hasRead) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.hasRead = hasRead;
+
+  this.id = 0;
 
   this.info = function() {
     console.log(`${this.title} by ${this.author}, ${this.pageCount}, ${this.hasRead ? "has read" : "has not read"}`);
@@ -15,14 +18,12 @@ function Book(title, author, pageCount, hasRead) {
 
 function addBookToLibrary(title, author, pageCount, hasRead) {
   let book = new Book(title, author, pageCount, hasRead);
+  book.id = booksMade++;
   myLibrary.push(book);
   insertNewBookHTML(book);
 
   //book.info();
 }
-
-addBookToLibrary("The Hoobbit", "J.R.R. Tolkien", 295, true);
-addBookToLibrary("Harry Pooter", "some biggot", 1111, false);
 
 myLibrary.forEach((book) => {
   book.info();
@@ -63,26 +64,80 @@ submitButton.addEventListener("click", (event) => {
 
 // INSERT NEW HTML
 
+const libraryGridContainer = document.querySelector('.bottomSectionProjectsGrid');
+
 function insertNewBookHTML(book) {
-  var toAdd = document.createDocumentFragment();
-  var newDiv = document.createElement('div');
-  newDiv.classList.add(`bottomSectionProjectsPanel`);
-  newDiv.innerHTML = 
+  var newPanelDiv = document.createElement('div');
+  newPanelDiv.classList.add(`bottomSectionProjectsPanel`);
+
+  var closeButtonDiv = document.createElement('div');
+  closeButtonDiv.classList.add('bottomSectionProjectsPanelCloseButton');
+
+  var closeButtonImg = document.createElement("img");
+  closeButtonImg.src = "img/close-circle-outline.svg";
+  closeButtonImg.classList.add("panelCloseButton");
+  closeButtonImg.addEventListener("click", () => {
+    console.log("hello");
+    libraryGridContainer.removeChild(newPanelDiv);
+  });
+
+  closeButtonDiv.appendChild(closeButtonImg);
+
+  var panelContentDiv = document.createElement('div');
+  panelContentDiv.classList.add('bottomSectionProjectsPanelContent');
+  panelContentDiv.innerHTML = 
   `
+  <h3>${book.title}</h3>
+  <h4>${book.author}</h4>
+  <p>${book.pageCount} pages</p>
+  <p>ID: ${book.id}</p>
+  <div class="bottomSectionProjectsPanelIcons">
+      <a href="https://sandboks.github.io/odin-calculator/">
+          <img src="img/cog.svg">
+      </a>
+      <a href="https://github.com/sandboks/odin-calculator">
+          <img src="img/star.svg">
+      </a>
+  </div>
+  `;
+
+  newPanelDiv.appendChild(closeButtonDiv);
+  newPanelDiv.appendChild(panelContentDiv);
+
+  /*
+  newPanelDiv.innerHTML = 
+  `
+  <div class="bottomSectionProjectsPanelCloseButton">
+    <img src="img/close-circle-outline.svg" class="panelCloseButton">
+  </div>
   <div class="bottomSectionProjectsPanelContent">
-      <h3>${book.title} / ${book.author}</h3>
+    <h3>${book.title}</h3>
+      <h4>${book.author}</h4>
       <p>${book.pageCount} pages</p>
+      <p>ID: ${book.id}</p>
       <div class="bottomSectionProjectsPanelIcons">
           <a href="https://sandboks.github.io/odin-calculator/">
-              <img src="img/eye-outline.svg">
+              <img src="img/cog.svg">
           </a>
           <a href="https://github.com/sandboks/odin-calculator">
-              <img src="img/github-mark.svg">
+              <img src="img/star.svg">
           </a>
       </div>
   </div>`;
-  toAdd.appendChild(newDiv);
-  document.querySelector('.bottomSectionProjectsGrid').appendChild(toAdd);
+  */
+
+  libraryGridContainer.appendChild(newPanelDiv);
+  
+  //const darkModeSwitch = document.querySelector("#darkModeToggle");
+  //darkModeSwitch.addEventListener("click", ToggleDarkMode)
+}
+
+function DeleteBook(index) {
+  console.log("test");
 }
 
 
+
+
+addBookToLibrary("The Hoobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("Harry Pooter", "some biggot", 1111, false);
